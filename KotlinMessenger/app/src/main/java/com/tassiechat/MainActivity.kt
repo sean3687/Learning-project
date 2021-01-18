0362007dabec4902b7ebf4fed0479d6c5790ddee
sean3687 <yonghyun.jin11@gmail.com>
2021-01-18 16:03:04 +0900

    2021-01-18

--- KotlinMessenger/app/src/main/java/com/tassiechat/MainActivity.kt
+++ KotlinMessenger/app/src/main/java/com/tassiechat/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tassiechat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,14 +11,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val email = email_edittext_register.text.toString()
-        val password = password_edittext_register.text.toString()
-
-
 
         register_button_register.setOnClickListener{
+            val email = email_edittext_register.text.toString()
+            val password = password_edittext_register.text.toString()
             Log.d("MainActivity", "Email is: " + email)
             Log.d("MainActivity", "Password: $password")
+
+        }
+
+        textview_account.setOnClickListener{
+            Log.d("MainActivity", "try to show login activity")
+            val  intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
