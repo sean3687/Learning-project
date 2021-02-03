ea65b40488ca563422f9b91996fc612472fd20d5
sean3687 <yonghyun.jin11@gmail.com>
2021-02-04 01:30:55 +0900

    02-04-2021

--- HolidayTracker 1/app/src/main/java/com/example/holidaytracker/MainActivity.kt
+++ HolidayTracker 1/app/src/main/java/com/example/holidaytracker/MainActivity.kt
@@ -11,69 +11,77 @@ import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    val displayListDate = mutableListOf<Long>()
+    val displayListName = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+        val holidaysList_date = arrayOf(
+            "01-01-2021 00:00:00",
+            "18-01-2021 00:00:00",
+            "31-05-2021 00:00:00",
+            "05-07-2021 00:00:00",
+            "07-09-2021 00:00:00",
+            "11-11-2021 00:00:00",
+            "25-11-2021 00:00:00",
+            "24-12-2021 00:00:00",
+            "31-12-2021 00:00:00"
+        )
+        val holidayList_name = arrayOf(
+            "New Year's Day",
+            "Martin Luther King Jr.Day",
+            "Memorial Day",
+            "Independence Day",
+            "Labor Day",
+            "Veterans Day",
+            "Thanksgiving",
+            "Christmas Day",
+            "New Year's Day"
+        )
 
-        val sdf = SimpleDateFormat("dd-MM-yyyy HH:mm:ss")
-        var countDate = ""   //""05-01-2022 00:00:00"
-        val now = Date()
-        Log.d("string", "1. $now") //1. Wed Feb 03 00:36:25 GMT 2021
+//making list
 
-        try
-        {
-            val date = sdf.parse(countDate) //Fri Jan 01 00:00:00 KST 2021
-            val currentTime = now.getTime()
-            val destinationDate = date.getTime()
-            Log.d("string", "2. $destinationDate") //2. 1641340800000
+//        val sdf = SimpleDateFormat("dd-MM-yyyy HH:mm:ss")
+//        var countDate = ""   //""05-01-2022 00:00:00"
+//        val now = Date()
+//        Log.d("string", "1. $now") //1. Wed Feb 03 00:36:25 GMT 2021
 
-            val StartCountDown = destinationDate - currentTime
-            Log.d("string", "3. $StartCountDown") //3. 29028082117
-            daysleft_title_main.start(StartCountDown)
+
+        for (i in 0..8) {
+            val sdf = SimpleDateFormat("dd-MM-yyyy HH:mm:ss")
+            val holidaysList = sdf.parse(holidaysList_date[i]).getTime()
+            val currentTime = Date()
+            val currentDate = currentTime.getTime()
+
+            if (holidaysList > currentDate) {
+
+                displayListDate.add(holidaysList)
+                displayListName.add(holidayList_name[i])
+
+            } else {
+
+            }
         }
 
-        catch (e: ParseException) {
+        try {
+
+            val StartCountDown = displayListDate[0] - Date().getTime()
+            Log.d("TAG2", "3. $StartCountDown") //3. 29028082117
+            holiday_title_main.setText(displayListName[0])
+            daysleft_title_main.start(StartCountDown)
+        } catch (e: ParseException) {
             e.printStackTrace()
         }
+
     }
 }
 
-fun main(args: Array<String>) {
-    val sdf = SimpleDateFormat("dd-MM-yyyy HH:mm:ss")
-    val holidaysList_date = arrayOf(
-        "01-01-2021 00:00:00",
-        "18-01-2021 00:00:00",
-        "31-05-2021 00:00:00",
-        "05-07-2021 00:00:00",
-        "07-09-2021 00:00:00",
-        "11-11-2021 00:00:00",
-        "25-11-2021 00:00:00",
-        "24-12-2021 00:00:00",
-        "31-12-2021 00:00:00"
-    )
-    val holidayList_name = arrayOf(
-        "New Year's Day",
-        "Martin Luther King Jr.Day",
-        "Memorial Day",
-        "Independence Day",
-        "Labor Day",
-        "Veterans Day",
-        "Thanksgiving",
-        "Christmas Day",
-        "New Year's Day"
-    )
-
-        val holidaysListParse = sdf.parse(holidaysList_date[0])
-        val destinationDate = holidaysListParse.getTime()
-        val currentTime = Date()
-        val currentDate = currentTime.getTime()
-        println(holidaysListParse)
-        println(destinationDate)
 
 
-    if (currentDate holidaysList_date )
-        (holidaysList_date[i])
 
-}
+
+
+
