4098807d32fe1c4a823bdec5863f6a7a316da9fb
sean3687 <yonghyun.jin11@gmail.com>
2021-02-17 11:10:00 +0900

    02-17-2021

--- DeathCounter/app/src/main/java/com/tassiecomp/deathcounter/SharedPreference.kt
+++ DeathCounter/app/src/main/java/com/tassiecomp/deathcounter/SharedPreference.kt
@@ -1,5 +1,6 @@
 package com.tassiecomp.deathcounter
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,12 @@ class SharedPreference : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shared_preference)
+
+        //SharedPreference
+        val sharedPreference = getSharedPreferences("sp1", Context.MODE_PRIVATE)
+        //mode
+        //- MODE_PRIVATE 생성한 application에서만 사용가능
+        //- MODE_WORLD_READABLE: 다른 어플리케이션에서 읽기가능
+        //- MODE-WORLD_WRITEABLE: 다른 어플리케이션에서 기록도 가능
     }
 }
