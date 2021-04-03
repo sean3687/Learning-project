4ad14040b7a99176cfbce0a49af5087af5bfa871
sean3687 <yonghyun.jin11@gmail.com>
2021-04-03 20:02:16 +0900

    04-03-2021

--- TodoList/app/src/main/java/com/tassiecomp/todolist/MainActivity.kt
+++ TodoList/app/src/main/java/com/tassiecomp/todolist/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.tassiecomp.todolist.databinding.ActivityMainBinding
 import com.tassiecomp.todolist.databinding.ItemTodoBinding
@@ -13,16 +14,36 @@ import com.tassiecomp.todolist.databinding.ItemTodoBinding
 private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var data = arrayListOf<Todo>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         val view = binding.root
         setContentView(view)
+
+        data.add(Todo("숙제",false))
+        data.add(Todo("청소", false))
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = TodoAdapter(data)
+
+        binding.addButton.setOnClickListener {
+            addTodo()
+
+        }
+
+    }
+
+    private fun addTodo(){
+        val todo = Todo(binding.editText.text.toString())
+        data.add(todo)
+        binding.recyclerView.
     }
 }
 
-data class Todo(val text: String, var isDone: Boolean)
+data class Todo(val text: String, var isDone: Boolean = false)
 
 
 class TodoAdapter(private val myDataset: List<Todo>) :
@@ -40,11 +61,7 @@ class TodoAdapter(private val myDataset: List<Todo>) :
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         holder.binding.todoText.text = myDataset[position].text
     }
-//    override fun onBindViewHolder(todoViewHolder: TodoViewHolder, position: Int) {
-//        holder.binding.todoText.text = myDataset[position].text
-//    }
 
-    // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount() = myDataset.size
 
 }
