4d5b28e400fdff5164cb396fc44bbf389547d095
sean3687 <yonghyun.jin11@gmail.com>
2021-04-23 16:24:08 +0900

    2021-04-23

--- MarsForecast/app/src/main/java/com/tassiecomp/airPolution/PhotoCollectionActivity.kt
+++ MarsForecast/app/src/main/java/com/tassiecomp/airPolution/PhotoCollectionActivity.kt
@@ -3,11 +3,17 @@ package com.tassiecomp.airPolution
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.tassiecomp.airPolution.model.Photo
+import com.tassiecomp.airPolution.recyclerview.PhotoGridRecyclerViewAdapter
+import kotlinx.android.synthetic.main.activity_photo_collection.*
 
 class PhotoCollectionActivity: AppCompatActivity() {
 
+    //데이터
     private var photoList = ArrayList<Photo>()
+    //어답터
+    private lateinit var photoGridRecyclerViewAdapter: PhotoGridRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +32,13 @@ class PhotoCollectionActivity: AppCompatActivity() {
 
         Log.d("TAG","PhotoCollectionActivity - onCreate() called / searchTerm: $searchTerm, photoList.count() : ${photoList.count()}")
 
+        this.photoGridRecyclerViewAdapter = PhotoGridRecyclerViewAdapter()
+
+        this.photoGridRecyclerViewAdapter.submitList(photoList)
+
+        my_photo_recycler_view.layoutManager = GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false)
+
+        my_photo_recycler_view.adapter = this.photoGridRecyclerViewAdapter
 
 
     } //
