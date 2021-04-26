823e1972002ce5d4086699d10cb9b8a66fa4ff5f
sean3687 <yonghyun.jin11@gmail.com>
2021-04-26 16:28:42 +0900

    2021-04-26

--- lonlatLocation/app/src/main/java/com/tassiecomp/lonlatlocation/MainActivity.kt
+++ lonlatLocation/app/src/main/java/com/tassiecomp/lonlatlocation/MainActivity.kt
@@ -1,11 +1,77 @@
 package com.tassiecomp.lonlatlocation
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
     }
+
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                1
+            )
+        } else {
+            getLocations()
+        }
+
+    }
+    @SuppressLint("MissingPermission")
+    private fun getLocations() {
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+            if(it == null){
+                Toast.makeText(this, "Error: can't load user location", Toast.LENGTH_SHORT).show()
+            }else it.apply {
+                val latitude = it.latitude
+                val longitude = it.longitude
+                textView.text = "latitude:$latitude longitude: $longitude"
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode ==1) {
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this,"Permission Granted", Toast.LENGTH_SHORT).show()
+                    getLocations()
+                }
+                else{
+                    Toast.makeText(this, "Permission Denied", Toast)
+                }
+            }
+        }
+    }
+
+
 }
+
